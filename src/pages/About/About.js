43f4a3e859815b180eebdf0d19767dfcc7848c14
unknown <limadanilo@06.com>
2020-07-30 08:56:32 -0300

    Adicionando a pagina de About

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import Menu from "../../components/Menu/Menu";
+import Footer from "../../components/Footer/Footer";
+
+import { MAbout } from "./aboutstyle";
+
 export default function About() {
     return (
-        <div>
-            About page
-        </div>
+        <MAbout>
+            <Menu />
+            <div>
+
+            <h1>Sobre o Styled Components</h1>
+            <p>styled-components é uma biblioteca para React e React Native que permite que você use estilos ao nível de componente na sua aplicação. Eles são escritos em uma mistura de JavaScript com CSS.
+Se você ainda não está familiarizado com styled-components, dê uma olhada no website oficial para ter uma idéia.</p>
+            </div>
+            <Footer />
+        </MAbout>
     )
 }
---
+++ src/pages/About/aboutstyle.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const MAbout = styled.div`
+
+    div{
+        
+        display:flex;
+        align-items: center;
+        justify-content:center;
+        flex-direction: column;
+    }
+
+    div h1{
+        font-size:30px;
+        margin: 95px 30px;  
+        text-align:center;    
+    }
+
+    div p{
+        width: 350px;
+        text-align:center;    
+    }
+
+
+    
+`;
