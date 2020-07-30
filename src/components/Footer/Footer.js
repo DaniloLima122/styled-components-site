5512bbfe0935f8b417d212b3b9c48e2be88df88f
unknown <limadanilo@06.com>
2020-07-30 08:28:11 -0300

    Adicionando o footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { MFooter } from "./footerstyle";
+
+export default function Footer() {
+    return (
+        <MFooter>
+            <p>Styled Components</p>
+        </MFooter>
+    )
+}
---
+++ src/components/Footer/footerstyle.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+
+export const MFooter = styled.footer`
+
+    background: #000;
+    height:150px;
+    width:100%;
+    margin-top:100px;
+    padding:70px 20px;
+    text-align: center;
+
+    p{
+        color:#fff;
+    }
+`;
