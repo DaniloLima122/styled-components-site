044891f57e3eeaf8cf29bdb42a825458baf362a5
unknown <limadanilo@06.com>
2020-07-31 13:55:21 -0300

    Finalizada a pagina Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,18 +4,40 @@ import Menu from "../../components/Menu/Menu";
 
 import Banner from "../../components/Banner/Banner";
 
-import {SectionText} from "./homestyle";
+import Card from "../../components/Card/Card";
+
+import Footer from "../../components/Footer/Footer";
+
+import { SectionText, SectionCards, GetStarted, Citacao } from "./homestyle";
 
 export default function Home() {
     return (
         <div>
-            <Menu/>
-            <Banner/>
+            <Menu />
+            <Banner />
             <SectionText>
                 <h1>Awesome Library</h1>
                 <p>Let's say you want to create a simple and reusable component that you can use throughout your application. There should be a normal version and a big and primary version for the important buttons. This is what it should look like when rendered: (this is a live example, click on them!)</p>
             </SectionText>
-            
+
+            <SectionCards>
+                <Card />
+                <Card />
+                <Card />
+            </SectionCards>
+
+            <GetStarted>
+                <button>Get started</button>
+            </GetStarted>
+
+            <Citacao>
+
+                <h2>
+                    <em>"Começe a usar hoje mesmo nos seus projetos"</em>
+                </h2>
+            </Citacao>
+
+            <Footer/>
 
         </div>
     )
--- src/pages/Home/homestyle.js
+++ src/pages/Home/homestyle.js
@@ -23,3 +23,47 @@ export const SectionText = styled.section`
     }
 
 `;
+
+
+export const SectionCards = styled.section`
+
+    margin:60px 40px;
+    height: 300px;
+    display:flex;
+    justify-content:center;
+`;
+
+
+export const GetStarted = styled.section`
+
+    height: 150px;
+    width:100%;
+    margin: 200px 0 100px 0;
+    background: #eee;
+    display: flex;
+    align-items:center;
+    justify-content:center;
+
+    button{
+        background: #000;
+        color:#fff;
+        font-size: 20px;
+        padding:10px 20px;
+        border:none;
+    }
+`;
+
+
+export const Citacao = styled.section`
+
+    text-align:center;
+
+    h2{
+
+        em{
+
+        color: #777;
+        }
+    }
+`; 
+
