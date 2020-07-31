86509ffd5631c2bed567f84962f43c0f3e004ff9
unknown <limadanilo@06.com>
2020-07-31 13:54:54 -0300

    Adicionando pagina de Contato

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
+
+import Menu from "../../components/Menu/Menu";
+import Footer from "../../components/Footer/Footer";
+
+import { MContact } from "./contactstyle";
+
 export default function Contact() {
     return (
-        <div>
-            Contato
-        </div>
+        <MContact>
+
+            <Menu />
+            <div>
+                <h2>Entre em contato conosco</h2>
+                <p>(00) 0 0000-0000</p>
+            </div>
+            <Footer />
+        </MContact>
     )
 }
---
+++ src/pages/Contact/contactstyle.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const MContact = styled.div`
+
+    div{
+        
+        display:flex;
+        align-items: center;
+        justify-content:center;
+        flex-direction: column;
+        margin:178px;
+    }
+
+    div h2{
+        font-size:30px;
+        margin: 20px 30px;  
+        text-align:center;    
+    }
+
+    div p{
+        width: 350px;
+        text-align:center;    
+    }
+
+
+    
+`;
